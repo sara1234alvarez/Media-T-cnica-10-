6ccf792a6728cc47ec0238304b841405ebcc21b3
sara1234alvarez <saravalencia123456789sara@gmail.com>
2024-05-16 15:17:18 -0500

    creación de código Stranger Things
    crear archivo programa para registrar encuentros con criaturas demogorgon

---
+++ programastrangerthings.cs.cs
@@ -0,0 +1,21 @@
+/*****************************************************************************
+  
+ Autor : Sara Alvarez - valeria mosquera 
+ Fecha : 2024-05-16
+ Descripción : Los personajes de stranger things necesitan un programa en c# 
+ para registrar los encuentros con criaturas del mundo como el demogorgon.
+ Completa el espacio en blanco para imprimir el nombre de la criatura en la 
+ consola 
+*******************************************************************************/
+using System;
+class program {
+  static void Main() {
+     string nombreCriatura = "Demogorgon";
+     
+     Console.WriteLine("el nombre de criatura es:" + nombreCriatura);
+  
+      
+  }
+  
+  
+}
