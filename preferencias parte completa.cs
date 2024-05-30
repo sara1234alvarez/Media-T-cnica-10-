f53b6e4e49ce449cc8874d8ba19725100d013db3
sara1234alvarez <saravalencia123456789sara@gmail.com>
2024-05-29 19:01:53 -0500

    preferencias de hombres y mujeres programa
    crear archivo programa sobre las preferencias de los hombres y mujeres mayores y menores de edad

---
+++ preferencias parte completa.cs
@@ -0,0 +1,58 @@
+/******************************************************************************
+Autor : sara alvarez 
+Fecha : 29-05-24
+Descripción : Ejercicio estructura de decisión múltiple
+Se desea un programa que ayude diciendo 
+las preferencias según el género:
+- Si es un hombre menor de edad sus preferencias son:
+    Figuras de acción, Caricaturas, Series TV
+- Si es un hombre mayor de edad sus preferencias son:
+    cine, picnic y filantropía
+- Si es una mujer menor de edad sus preferencias son:
+    Muñecas, Maquillaje, Series TV
+- Si es una mujer mayor de edad sus preferencias son:
+    Maquillaje, Series TV, Cine, filantropía
+
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main(string[]args){
+    Console.WriteLine("digite si eres hombre o mujer ");
+    string genero = Console.ReadLine();
+    
+    Console.WriteLine("ingrese su edad");
+    int edad = int.Parse(Console.ReadLine());
+    
+    if(genero == "hombre"){
+    
+        if (edad < 18)
+           {
+           Console.WriteLine("tus preferencias son: Figuras de acción, Caricaturas, Series TV");
+           }
+          
+           else
+            {
+                Console.WriteLine("Sus preferencias son: Cine, Picnic, Filantropía");
+            }
+       
+    } 
+       
+          
+        if (genero == "mujer"){
+        
+            if (edad < 18)
+                
+            {
+               Console.WriteLine("tus preferencias son: Muñecas, Maquillaje, Series TV");
+            } 
+            
+           else
+            {
+                Console.WriteLine("preferencias son: Maquillaje, Series TV, Cine, filantropía");
+            }
+        
+        }    
+        
+  }      
+}
