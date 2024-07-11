675d9242e6f0cf36f281dfaf7fa6ab0b2a426a7e
sara1234alvarez <saravalencia123456789sara@gmail.com>
2024-07-11 16:09:51 -0500

    Vector Harry Potter
    Archivo programa de Harry Potter

---
+++ VectorHarry Potter.cs
@@ -0,0 +1,56 @@
+
+
+/******************************************************************************
+
+Autor:          Sara Alvarez 
+
+Fecha:          11/07/2024
+
+Descripción:    Harry Potter necesita un programa 
+                en C# para listar sus hechizos. 
+                Completa el espacio en blanco 
+               para mostrar los nombres de los 
+               hechizos en la consola:
+
+*******************************************************************************/
+using System;
+
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] hechizos = { "Expecto Patronum", "Wingardium Leviosa", "Expelliarmus" };
+        
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMWN0KWMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMWWNxdKWMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMXxdKWMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMXxdKWMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMWXxdKWMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMWKxxKWMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMW0dkXWMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMMNKOdkXMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMMNOodkXMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMMNOddkXWMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMMMNkdOKXWMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMMW0ocxXMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMMWk:;lOWMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMMM0lcoONMMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMMWKolxXWMMMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMWKolkXMMMMMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMWXxllxXMMMMMMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMM0:;:o0MMMMMMMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMKdlcxNMMMMMMMMMMMMMMMMMMMMMMMMMM");
+        Console.WriteLine("MMMMMMMMMMWNNMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+        Thread.Sleep(3000);
+        Console.Clear();
+        
+
+       
+    
+    for (int i = 0; i <= 2; i++) {
+        Console.WriteLine(hechizos[i]);
+    }
+    }
+}
+
